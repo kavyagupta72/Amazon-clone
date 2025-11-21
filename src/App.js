023c2169b8af524664bf24ac01c4015475b3169a
Kavya Gupta <kavyaaaaaa72@gmail.com>
2025-11-21 23:14:14 +0530

    day-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      {/*header*/}
-      <Header />
-      {/*Home*/}
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+
+          <Route path="/" element={[<Header />, <Home />]} />
+
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import "./Header.css";
+import { Link } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
   return (
     <div className = "header">
-        <img
-            className = "header__logo"
-            src = "https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        />
+        <Link to='/'>
+            <img
+                className = "header__logo"
+                src = "https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            />
+        </Link>
         <div className = "header__search">
             <input
                 className="header__searchInput" type="text" />
