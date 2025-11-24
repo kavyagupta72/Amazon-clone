00d904ed7e08f4efbe943a183859f1d3401a250b
Kavya Gupta <kavyaaaaaa72@gmail.com>
2025-11-24 10:43:02 +0530

    Day-03

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Subtotal from './Subtotal';
 import './Checkout.css';
 function Checkout() {
   return (
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Subtotal.css';
-import CurrencyFormat from 'react-currency-imput-field';
+import CurrencyFormat from 'react-currency-input-field';
 
 function Subtotal() {
   return (
     <div className = 'subtotal'>
-      
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 Items) : <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox"/>This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix='Rs. '
+      />
+      <button>Proceed to Checkout</button> 
     </div>
   )
 }
