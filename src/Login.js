712524106fc9672db1581f54e0797b15343651c1
Kavya Gupta <kavyaaaaaa72@gmail.com>
2025-12-10 11:23:18 +0530

    built the frontend for sign-in page

--- src/Login.js
+++ src/Login.js
@@ -7,9 +7,33 @@ function Login() {
     <div className="login">
         <Link to="/">
         <img className='login__logo'
-            src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+            src="https://www.pngall.com/wp-content/uploads/13/Amazon-Logo-PNG.png" 
         />
         </Link>
+        <div className='login__container'>
+          <h1>
+            Sign-In
+          </h1>
+          <form>
+            <h5>E-Mail</h5>
+            <input type='text' />
+
+            <h5>Password</h5>
+            <input type='password' />
+
+            <button className='login__signInButton' type='submit'>
+              Sign In
+            </button>
+
+            <p>
+              By signing-in you agree to Amazon's Fake Clone's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+            </p>
+
+            <button className="login__registerButton">
+              Create Your Amazon Account
+            </button>
+          </form>
+        </div>
     </div>
   )
 }
